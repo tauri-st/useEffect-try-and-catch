2b078881b034ca663930b59fc23fbffd9d68fd19
tauri_s <tauristclaire@gmail.com>
2023-12-20 13:15:42 -0800

    Add fetch function to try block to fetch

--- src/Friend.js
+++ src/Friend.js
@@ -7,7 +7,7 @@ const Friend = () => {
   useEffect(() => {
     const fetchData = async () =>
       try {
-
+        const request = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/8231fb5350d726663f2bb16af7cf6945/raw/0dde3b3942214ed39a08479b334c752bea05b3a6/friend-status.json");
       } catch () {
 
       }
