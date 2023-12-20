58914a1452a69818f12746aac82104b86345a8ad
tauri_s <tauristclaire@gmail.com>
2023-12-20 12:51:05 -0800

    Starter code and dependencies

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import Friend from "./Friend";
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
---
+++ src/Friend.js
@@ -0,0 +1,13 @@
+const Friend = () => {
+  return (
+    <div className="friend-card">
+      <div className="friend-details">
+        <div className="avatar"></div>
+        <h2 className="friend-name"></h2>
+      </div>
+      <div className="friend-status"></div>
+    </div>
+  );
+};
+
+export default Friend;
