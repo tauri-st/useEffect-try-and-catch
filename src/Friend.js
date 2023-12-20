b0458536b45a5bf07bb7a55b281a6c684ecef7e6
tauri_s <tauristclaire@gmail.com>
2023-12-20 13:32:45 -0800

    Move state and useEffect hooks from Friend component to App component

--- src/Friend.js
+++ src/Friend.js
@@ -1,23 +1,4 @@
-import React, {useState, useEffect} from "react";
-
 const Friend = () => {
-  
-  const [friendData, setFriendData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const request = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/8231fb5350d726663f2bb16af7cf6945/raw/0dde3b3942214ed39a08479b334c752bea05b3a6/friend-status.json");
-        const friendStatus = await request.json();
-        setFriendData(friendStatus);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-      fetchData();
-  }, []);
-
-  console.log(friendData);
   
   return (
     <div className="friend-card">
