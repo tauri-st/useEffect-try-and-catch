6849f69371b359b93e24aaf4d7eb6c4dbb53937f
tauri_s <tauristclaire@gmail.com>
2023-12-20 13:59:17 -0800

    Add id as key for friend component and pass friend object to friend component as a prop

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <div className="App">
       {friendData.map((friend) => {
-        return <Friend />
+        return <Friend key={id} friend={friend}/>
       })};
     </div>;
   );
