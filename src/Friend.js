75d25a8bb2844875ad157c4f80ca9bee427b45a3
tauri_s <tauristclaire@gmail.com>
2023-12-20 13:29:29 -0800

    Fix fetchData function to be scoped in brackets

--- src/Friend.js
+++ src/Friend.js
@@ -5,7 +5,7 @@ const Friend = () => {
   const [friendData, setFriendData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () =>
+    const fetchData = async () => {
       try {
         const request = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/8231fb5350d726663f2bb16af7cf6945/raw/0dde3b3942214ed39a08479b334c752bea05b3a6/friend-status.json");
         const friendStatus = await request.json();
@@ -13,6 +13,7 @@ const Friend = () => {
       } catch (error) {
         console.log(error);
       }
+    }
       fetchData();
   }, []);
 
