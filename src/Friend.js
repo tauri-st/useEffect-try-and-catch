9bce0c6aa7f029152cb4e22973b785e0c0a11f20
tauri_s <tauristclaire@gmail.com>
2023-12-20 14:14:50 -0800

    Display name of friend object in returned JSX of Friend component

--- src/Friend.js
+++ src/Friend.js
@@ -4,7 +4,7 @@ const Friend = ( {friend} ) => {
     <div className="friend-card">
       <div className="friend-details">
         <div className="avatar"></div>
-        <h2 className="friend-name"></h2>
+        <h2 className="friend-name">{friend.name}</h2>
       </div>
       <div className="friend-status"></div>
     </div>
