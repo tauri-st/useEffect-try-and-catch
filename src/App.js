857c2614b08a2e13f6b923e44f3d98c40670ca10
tauri_s <tauristclaire@gmail.com>
2023-12-20 14:22:15 -0800

    Fix assignment of key for Friend component to reference friend object

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <div className="App">
       {friendData.map((friend) => {
-        return <Friend key={id} friend={friend}/>
+        return <Friend key={friend.id} friend={friend}/>
       })};
     </div>
   );
