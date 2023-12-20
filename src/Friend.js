314dfeb7734281fef148222f8731d5e7a20a02ae
tauri_s <tauristclaire@gmail.com>
2023-12-20 14:52:38 -0800

    Display captured first letter in avatar div of JSX of Friend component

--- src/Friend.js
+++ src/Friend.js
@@ -2,12 +2,10 @@ const Friend = ( {friend} ) => {
   
   const letterAvatar = friend.name.split("")[0]
 
-  console.log(letterAvatar);
-
   return (
     <div className="friend-card">
       <div className="friend-details">
-        <div className="avatar"></div>
+        <div className="avatar">{letterAvatar}</div>
         <h2 className="friend-name">{friend.name}</h2>
       </div>
       <div className={`friend-status ${friend.online ? "online" : "offline"}`}></div>
